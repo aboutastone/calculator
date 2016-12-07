deaea7bc620c50990bbef504e1282bfe6c6db82c
chang_feng <chang_feng@apple.com>
2016-12-07 17:05:13 +0000

    first commit

---
+++ src/main/scala/calculator/Polynomial.scala
@@ -0,0 +1,20 @@
+package calculator
+
+object Polynomial {
+  def computeDelta(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double]): Signal[Double] = {
+    Signal(b() * b() - 4 * a() * c())
+  }
+
+  def computeSolutions(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    Signal(
+      if (!computeDelta(a, b, c)().equals(0)) {
+        Set((-b() + math.sqrt(computeDelta(a, b, c)())) / (2 * a()),
+          (-b() - math.sqrt(computeDelta(a, b, c)())) / (2 * a()))
+      } else {
+        Set((-b() / (2 * a())))
+      }
+    )
+  }
+}
